9fdd2dea3994653da892dbc11029affacebc1356
parthan-murali <73218391+parthan-murali@users.noreply.github.com>
2020-10-21 16:03:48 +0800

    Added hey guys

--- src/com/oops/User.java
+++ src/com/oops/User.java
@@ -2,6 +2,6 @@ package com.oops;
 
 public class User {
     public static void main(String[] args) {
-        System.out.print("Ok so let's start");
+        System.out.print("Hey Guys");
     }
 }
