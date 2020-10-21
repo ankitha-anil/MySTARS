7aee57eb8f3f677d666324fc5f6ffe5515df1b67
ankithaanil <ankitha001@e.ntu.edu.sg>
2020-10-21 15:07:59 +0800

    Removed Main class and made it User class.

--- src/com/oops/Main.java
+++
@@ -1,7 +0,0 @@
-package com.oops;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.print("Ok so let's start");
-    }
-}
--- src/com/oops/User.java
+++ src/com/oops/User.java
@@ -1,4 +1,7 @@
 package com.oops;
 
 public class User {
+    public static void main(String[] args) {
+        System.out.print("Ok so let's start");
+    }
 }
