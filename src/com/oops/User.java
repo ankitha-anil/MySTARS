51835c6f94047165cec35a4d5d7d6fb1af9fe58c
Srishti <srishti.arora72@gmail.com>
2020-10-22 14:52:55 +0800

    added get set

--- src/com/oops/User.java
+++ src/com/oops/User.java
@@ -4,7 +4,37 @@ public class User {
     protected String name;
     protected String id;
     private String password; //if we are using the login function here
+
+    public User(String name,String id,String password){
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    }
+
+    public void setName(String name){
+        this.name = name;
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setPassword(String pass){
+        this.password = pass;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getPassword(){
+        return password;
+    }
     public static void main(String[] args) {
         System.out.print("Hey Guys");
     } //test
 }
+
