9e47791afb91ef151e137db02a5cee0be1f4621f
Atul-Acharya-17 <61643403+Atul-Acharya-17@users.noreply.github.com>
2020-11-16 16:28:23 +0800

    Update Admin.java

--- src/Entity/Admin.java
+++ src/Entity/Admin.java
@@ -6,8 +6,12 @@ public class Admin extends User implements Serializable {
 
     // Create some attribute
 
-    Admin(String name, String networkName,String password, String email, String gender, String nationality) {
-        super(name, networkName,password,email, gender, nationality);
+    Admin(String name, String networkName, String gender, String nationality, String emailID) {
+        super(name, networkName, gender, nationality, emailID);
+    }
+    public Admin(String networkName){
+        super(networkName);
     }
 
 }
+
