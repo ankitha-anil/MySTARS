2cdba0c7f0b6b62223b18cf28afe614818e9f99e
Atul-Acharya-17 <61643403+Atul-Acharya-17@users.noreply.github.com>
2020-11-16 16:33:13 +0800

    Update LoginMgr.java

--- src/Controller/LoginMgr.java
+++ src/Controller/LoginMgr.java
@@ -6,74 +6,113 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class LoginMgr {
-    public static final String SEPARATOR = "|";
-
+    private static final String SEPARATOR = "|";
+    private static final String fileName = "users.txt";
+    final static String SALT = "oops_project";
+    private static ArrayList<String> loginDetails = new ArrayList<>();
 
     //Reading User file
-    public static ArrayList readUser(String filename) throws IOException {
-        // read String from text file
-        ArrayList stringArray = (ArrayList) read(filename);
-        ArrayList alr = new ArrayList() ;// to store Users data
+    public LoginMgr() throws IOException {
+        loginDetails = (ArrayList<String>) readCredentials(fileName);
+    }
 
-        for (int i = 0 ; i < stringArray.size(); i++) {
-            String st = (String)stringArray.get(i);
+    public static boolean loginCheck(String userName, String password) throws IOException {
+
+        for (int i = 0; i < loginDetails.size(); i++) {
+            String record = loginDetails.get(i);
             // get individual 'fields' of the string separated by SEPARATOR
-            StringTokenizer star = new StringTokenizer(st , SEPARATOR);	// pass in the string to the string tokenizer using delimiter ","
+            StringTokenizer star = new StringTokenizer(record, SEPARATOR);    // pass in the string to the string tokenizer using delimiter ","
 
-            String  username = star.nextToken().trim();	// first token
-            String  password = star.nextToken().trim();	// second token
+            String userNameFile = star.nextToken().trim();    // first token
+            String passwordFile = star.nextToken().trim();    // second token
+            String saltedPassword = SALT + password;
+            String hashedPassword = generateHash(saltedPassword);
 
-            // create User object from file data
-            User user = new User(username,password);
-            // add to User list
-            alr.add(user) ;
+            if (userName.equals(userNameFile) && hashedPassword.equals(passwordFile)) {
+                return true;
+            }
         }
-        return alr ;
+        return false;
     }
 
 
-    /** Read the contents of the given file. */
-    public static List read(String fileName) throws IOException {
-        List data = new ArrayList() ;
-        Scanner scanner = new Scanner(new FileInputStream(fileName));
+    /**
+     * Read the contents of the given file.
+     *
+     * @param fileName
+     */
+    public static List readCredentials(String fileName) throws IOException {
+        List data = new ArrayList();
+        Scanner scanner = new Scanner(new FileInputStream(LoginMgr.fileName));
         try {
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 data.add(scanner.nextLine());
             }
-        }
-        finally{
+        } finally {
             scanner.close();
         }
         return data;
     }
 
+    public static void saveUser() throws IOException {
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName));
+        try {
+            for (int i = 0; i < loginDetails.size(); i++) {
+                printWriter.println((String) loginDetails.get(i));
+            }
+        } finally {
+            printWriter.close();
+        }
+    }
+
 
     //  Saving the User class for username file
-    public static void saveUser(String filename, List al) throws IOException {
-        List alw = new ArrayList() ;// to store Users data
+    public static void createUser(User newUser, String password) throws IOException {
 
-        for (int i = 0 ; i < al.size() ; i++) {
-            User u = (User)al.get(i);
-            StringBuilder st =  new StringBuilder() ;
-            st.append(u.getNetworkName().trim());
-            st.append(SEPARATOR);
-            st.append(u.getPassword());
-            alw.add(st.toString()) ;
+        for (int i = 0; i < loginDetails.size(); i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(newUser.getNetworkName().trim());
+            stringBuilder.append(SEPARATOR);
+            stringBuilder.append(generateHash(password));
+            loginDetails.add(stringBuilder.toString());
         }
-        PrintWriter out = new PrintWriter(new FileWriter(filename));
+        PrintWriter printWriter = new PrintWriter(new FileWriter(fileName));
         try {
-            for (int i =0; i < alw.size() ; i++) {
-                out.println((String)alw.get(i));
+            for (int i = 0; i < loginDetails.size(); i++) {
+                printWriter.println((String) loginDetails.get(i));
             }
+        } finally {
+            printWriter.close();
         }
-        finally {
-            out.close();
+    }
+
+    public static String generateHash(String input) {
+        StringBuilder hash = new StringBuilder();
+        try {
+            MessageDigest sha = MessageDigest.getInstance("SHA-1");
+            byte[] hashedBytes = sha.digest(input.getBytes());
+            char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+            for (int idx = 0; idx < hashedBytes.length; idx++) {
+                byte b = hashedBytes[idx];
+                hash.append(digits[(b & 0xf0) >> 4]);
+                hash.append(digits[b & 0x0f]);
+            }
+        } catch (
+                NoSuchAlgorithmException e) {
+            // handle error here.
         }
+        return hash.toString();
+    }
+
+    public static ArrayList<String> getLoginDetails() {
+        return loginDetails;
     }
 }
