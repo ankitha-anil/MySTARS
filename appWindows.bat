d76277a801c4136d1bc438f88be121b0e7b95553
Atul-Acharya-17 <61643403+Atul-Acharya-17@users.noreply.github.com>
2020-12-01 01:00:44 +0800

    Update appWindows.bat

--- appWindows.bat
+++ appWindows.bat
@@ -1,5 +1,5 @@
 @echo off
 cd src
-javac -d ../classes -cp ../jars/javax.mail.jar;../jars/activation.jar entity/*.java actor/*.java controller/*.java
+javac -d ../classes -cp ../jars/javax.mail.jar;../jars/activation.jar entity/*.java controller/*.java boundary/*.java
 cd ../classes
 java -cp ../jars/javax.mail.jar;../jars/activation.jar; boundary.MyStarsInterface
