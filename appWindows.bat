ea9c15e916ea46b2cc068ab6e2c68ef20dd33e7f
Atul-Acharya-17 <61643403+Atul-Acharya-17@users.noreply.github.com>
2020-12-01 01:02:26 +0800

    Delete appWindows.bat

--- appWindows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd src
-javac -d ../classes -cp ../jars/javax.mail.jar;../jars/activation.jar entity/*.java controller/*.java boundary/*.java
-cd ../classes
-java -cp ../jars/javax.mail.jar;../jars/activation.jar; boundary.MyStarsInterface
